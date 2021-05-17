a410a77b96068b88a7dd9cb10ae005e920453e56
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2021-05-17 18:21:46 +0530

    Initial commit

---
+++ lesson1/task1/test/Tests.kt
@@ -0,0 +1,11 @@
+import org.junit.Assert
+import org.junit.Test
+
+class Test {
+    @Test
+    fun testSolution() {
+        Assert.assertEquals(
+                "You should say hello to Kotlin",
+                "Hello, Kotlin!", hello())
+    }
+}
